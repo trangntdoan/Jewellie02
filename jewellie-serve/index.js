7dc18c12addbcc2e2cbf7a412858f28a0b377e5b
thijlinh <linhntt19406@st.uel.edu.vn>
2022-05-15 15:04:00 +0700

    connect

--- jewellie-serve/index.js
+++ jewellie-serve/index.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 //Connect DB
 const db = require('./config/db');
 db.connect();
+
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(port, () => {
+    console.log(`My serve listing on port ${port}`)
+})
+
